708f628071a6db4c3b180adac484863670d560cb
Manel Pavon <manelpavon10@gmail.com>
2017-08-14 16:47:52 +0200

    converted Header into functional component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,40 +4,38 @@ import AppBar from "material-ui/AppBar";
 import TextField from "material-ui/TextField";
 import RaisedButton from "material-ui/RaisedButton";
 
-class Header extends Component {
-	render() {
-		const buttons = (
-			<div>
-				<TextField
-					type="text"
-					inputStyle={{ color: "white" }}
-					placeholder="Search in your own collection ..."
-					onChange={this.props.inputChange}
-					value={this.props.inputValue}
-				/>
-			</div>
-		);
-
-		const links = (
-			<Link to="/favourites">
-				<RaisedButton label="Favourite Movies" primary={true} fullWidth={true} />
-			</Link>
-		);
-
-		const renderButtons = this.props.internalSearch ? buttons : links;
-
-		return (
-			<AppBar
-				title={
-					<Link to="/" style={{ textDecoration: "none", color: "white" }}>
-						HOME
-					</Link>
-				}
-				showMenuIconButton={false}
-				iconElementRight={renderButtons}
+const Header = props => {
+	const buttons = (
+		<div>
+			<TextField
+				type="text"
+				inputStyle={{ color: "white" }}
+				placeholder="Search in your own collection ..."
+				onChange={props.inputChange}
+				value={props.inputValue}
 			/>
-		);
-	}
-}
+		</div>
+	);
+
+	const links = (
+		<Link to="/favourites">
+			<RaisedButton label="Favourite Movies" primary={true} fullWidth={true} />
+		</Link>
+	);
+
+	const renderButtons = props.internalSearch ? buttons : links;
+
+	return (
+		<AppBar
+			title={
+				<Link to="/" style={{ textDecoration: "none", color: "white" }}>
+					HOME
+				</Link>
+			}
+			showMenuIconButton={false}
+			iconElementRight={renderButtons}
+		/>
+	);
+};
 
 export default Header;
